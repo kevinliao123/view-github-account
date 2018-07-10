f7f9ae61af925c38a3c093f6d143c028f9298aa8
Kevin Liao <he88@vt.edu>
2018-07-10 15:20:01 -0400

    create validation util

---
+++ app/src/main/java/kevinliao/com/viewgithub/util/ValidateUtils.java
@@ -0,0 +1,17 @@
+package kevinliao.com.viewgithub.util;
+
+import java.util.regex.Pattern;
+
+import static android.support.v4.util.PatternsCompat.EMAIL_ADDRESS;
+
+public class ValidateUtils {
+    public static boolean isValidateEmail(String email) {
+        return EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    public static boolean isValidatePassword(String password) {
+        return Pattern.compile(
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{4,}$"
+        ).matcher(password).matches();
+    }
+}
---
+++ app/src/test/java/kevinliao/com/viewgithub/util/ValidateUtilsTest.java
@@ -0,0 +1,22 @@
+package kevinliao.com.viewgithub.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidateUtilsTest {
+
+    @Test
+    public void isValidateEmail() {
+        assertTrue(ValidateUtils.isValidateEmail("github@gmail.com"));
+        assertFalse(ValidateUtils.isValidateEmail("githubgmail.com"));
+        assertFalse(ValidateUtils.isValidateEmail("github@gmailcom"));
+    }
+
+    @Test
+    public void isValidatePassword() {
+        assertTrue(ValidateUtils.isValidatePassword("Github1"));
+        assertFalse(ValidateUtils.isValidatePassword("Github"));
+        assertFalse(ValidateUtils.isValidatePassword("github1"));
+    }
+}
